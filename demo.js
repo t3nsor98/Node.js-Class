1006423db0b19b22272b943c860ad30ed9552999
Digbijaya Lenka <digbijayalenka@gmail.com>
2025-04-13 00:09:01 +0530

    add Human class with constructor and instance creation

---
+++ demo.js
@@ -0,0 +1,11 @@
+class Human {
+    constructor(name, age, salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+}
+
+let digbijaya = new Human("Digbijaya", 24, 60000);
+
+console.log(digbijaya.salary);
